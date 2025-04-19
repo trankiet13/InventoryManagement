4257efb7dda6bbef7a19d0266c9343b16d84bcc9
NgocChi <ngocchi1010001@gmail.com>
2025-04-19 11:47:03 +0700

    Update UserBl.cs

--- InventoryManagement/UserBl.cs
+++ InventoryManagement/UserBl.cs
@@ -1,4 +1,5 @@
-﻿using DataLayer;
+using DataLayer;
+using System;
 using System.Collections;
 using System.Data;
 
@@ -11,12 +12,23 @@ namespace BusinessLayer
         // Hàm lọc người dùng (chứa nghiệp vụ tìm kiếm dữ liệu)
         public DataTable GetUsersByName(string keyword)
         {
-            string query = $@"
-                SELECT IDUSER, USERNAME, PASSWD, FULLNAME, MACTY,MADVI, LAST_PWD_CHANGED,DISABLED,ISGROUP 
-                FROM tb_SYS_USER 
-                WHERE USERNAME LIKE '%{keyword}%' 
-                ORDER BY IDUSER";
-            return userDL.GetDataTable(query);
+            string query = @"
+        SELECT IDUSER, USERNAME, PASSWD, FULLNAME, MACTY, MADVI, LAST_PWD_CHANGED, DISABLED, ISGROUP 
+        FROM tb_SYS_USER 
+        WHERE USERNAME LIKE @keyword 
+        ORDER BY IDUSER";
+
+            Hashtable parameters = new Hashtable();
+            parameters.Add("@keyword", $"%{keyword}%");
+
+            return userDL.GetDataTable(query, parameters);
+        }
+        public DataTable GetUserById(int userId)
+        {
+            string query = "SELECT * FROM tb_SYS_USER WHERE IDUSER = @userId";
+            Hashtable parameters = new Hashtable();
+            parameters.Add("@userId", userId);
+            return userDL.GetDataTable(query, parameters);
         }
         public int DeleteUser(int id)
         {
@@ -28,20 +40,32 @@ namespace BusinessLayer
 
         public int SaveUser(int id, string username, string fullname, string pass, string macty, string madvi, int role)
         {
-
+            if (IsUsernameExists(username, id == 0 ? (int?)null : id))
+            {
+                throw new Exception("Username đã tồn tại trong hệ thống!");
+            }
             return userDL.InsertOrUpdateUser(id, username, fullname, pass, macty, madvi, role);
         }
-        //public int SaveUser(int id, string username, string password, string fullname, string madvi, string macty)
-        //{
-        //    if (id == 0)
-        //    {
-        //        return userDL.InsertUser(username, password, fullname, madvi, macty);
-        //    }
-        //    else
-        //    {
-        //        return userDL.UpdateUser(id, username, password, fullname, madvi, macty);
-        //    }
-        //}
+        public bool IsUsernameExists(string username, int? currentUserId = null)
+        {
+            string query = "SELECT COUNT(*) FROM tb_SYS_USER WHERE USERNAME = @username";
+
+            if (currentUserId != null)
+            {
+                query += " AND IDUSER != @currentUserId"; // Loại trừ user hiện tại khi cập nhật
+            }
+
+            Hashtable parameters = new Hashtable();
+            parameters.Add("@username", username);
+
+            if (currentUserId != null)
+            {
+                parameters.Add("@currentUserId", currentUserId.Value);
+            }
+
+            int count = Convert.ToInt32(userDL.MyExecuteScalar(query, CommandType.Text, parameters));
+            return count > 0;
+        }
 
 
 
